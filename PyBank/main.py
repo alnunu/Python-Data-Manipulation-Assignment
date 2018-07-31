b68e19a39ce02f7652634bb4a3c78acf2ec9f789
Socrates <alnunu0@gmail.com>
2018-07-31 10:42:32 -0500

    Python Hmwk 3 PyBank changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,38 +2,45 @@ import os
 import csv
 
 # Assigning initial variables
-pl_betweenmonths = []
-month_count=0
-total_pl=0
-avg_change =0
-greatest_profit = ["month", 0]
-largest_loss = ["month", 0]
+month_count = 0
+total_pl = 0
+rev_list = []
+revdif = []
+date_list = []
 path_file = '../alnunu/repos/python-challenge/budget_data.csv'
 
 # Open and read csv
 with open(path_file, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-
     # Read the header row first (skip this part if there is no header)
     csv_header = next(csvfile)
-
     # Read through each row of data after the header
     for row in csvreader:
         # Count the total number of months
         month_count = month_count+1
         # Calculate total profit and loss
         total_pl=total_pl + int(row[1])
-        # determing greatest profit and largest loss
-        if int(row[1]) > greatest_profit[1]:
-            greatest_profit = [row[0],int(row[1])]
-        if int(row[1]) < largest_loss[1]:
-            largest_loss = [row[0],int(row[1])]
+        # Create list of revenues and dates to calculate changes later
+        rev_list.append(row[1])
+        date_list.append(row[0])
+        
+# Create difference in revenue list
+for i in range(1,len(rev_list)):
+    revdif.append(int(rev_list[i]) - int(rev_list[i-1])) 
+    #calculate average change 
+    avg_change = sum(revdif)/len(revdif)
+    #calculate max and min revenue change values
+    greatest_profit = max(revdif)
+    largest_loss = min(revdif)
+    #retrieve date
+    greatest_profit_month = str(date_list[revdif.index(max(revdif))])
+    largest_loss_month = str(date_list[revdif.index(min(revdif))])
 
 #Print Analysis
 print("Financial Analysis")
 print("----------------------------")
 print("Total Months: " + str(month_count))
 print("Total: " + "$" + str(total_pl))
-print("Average Change: " + "$" + str(avg_change))
-print("Greatest Increase in Profits: " + str(greatest_profit[0]) + " ($" + str(greatest_profit[1])+ ")")
-print("Greatest Decrease in Profits: " + str(largest_loss[0]) + " ($" + str(largest_loss[1])+ ")")
+print("Average Change: " + "$" + (str(avg_change)))
+print("Greatest Increase in Profits: " + greatest_profit_month + " ($" + str(greatest_profit)+ ")")
+print("Greatest Decrease in Profits: " + largest_loss_month + " ($" + str(largest_loss)+ ")")
