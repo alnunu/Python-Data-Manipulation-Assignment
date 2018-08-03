18826a4eab8cd7626bac970267d35aef41992701
Socrates <alnunu0@gmail.com>
2018-08-03 16:03:00 -0500

    test

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,7 +12,7 @@ path_file = '/Users/alnunu/repos/python-challenge/budget_data.csv'
 # Open and read csv
 with open(path_file, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    # Read the header row first (skip this part if there is no header)
+    # Read the header row first
     csv_header = next(csvfile)
     # Read through each row of data after the header
     for row in csvreader:
@@ -20,7 +20,7 @@ with open(path_file, newline="") as csvfile:
         month_count = month_count+1
         # Calculate total profit and loss
         total_pl=total_pl + int(row[1])
-        # Create list of revenues to calculate change later
+        # Create list of revenues to calculate change
         rev_list.append(row[1])
         date_list.append(row[0])
 
@@ -32,7 +32,7 @@ for i in range(1,month_count):
     #calculate max and min revenue change values
     greatest_profit = max(revdif)
     largest_loss = min(revdif)
-    #retrieve date
+    #retrieve month
     greatest_profit_month = str(date_list[revdif.index(max(revdif))+1])
     largest_loss_month = str(date_list[revdif.index(min(revdif))+1])
 
@@ -40,7 +40,18 @@ for i in range(1,month_count):
 print("Financial Analysis")
 print("----------------------------")
 print("Total Months: " + str(month_count))
-print("Total: " + "$" + str(total_pl))
-print("Average Change: " + "$" + (str(avg_change)))
+print("Total: $" + str(total_pl))
+print("Average Change: $" + (str(avg_change)))
 print("Greatest Increase in Profits: " + greatest_profit_month + " ($" + str(greatest_profit)+ ")")
-print("Greatest Decrease in Profits: " + largest_loss_month + " ($" + str(largest_loss)+ ")")
+print("Greatest Decrease in Profits: " + largest_loss_month+ " ($" + str(largest_loss)+ ")")
+
+#create txt file
+file = open("PyBank.txt","w")
+file.write("Financial Analysis \n")
+file.write("----------------------------\n")
+file.write("Total Months: str(month_count)\n")
+file.write("Total: $" + str(total_pl)+ "\n")
+file.write("Average Change: $" + (str(avg_change)) + "\n")
+file.write("Greatest Increase in Profits: " + greatest_profit_month + " ($" + str(greatest_profit)+ ")\n")
+file.write("Greatest Decrease in Profits: " + largest_loss_month+ " ($" + str(largest_loss)+ ")")
+file.close()
